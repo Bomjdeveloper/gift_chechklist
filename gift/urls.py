2f5c97b8f469f71b026210498ad70491cbaab875
Bomjdeveloper <100873915+Bomjdeveloper@users.noreply.github.com>
2022-12-25 18:23:03 +0200

    Add files via upload

---
+++ gift/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'gift'
+urlpatterns = [
+    path('home', views.index, name="index"),
+    path('registration', views.registration, name="registration"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout_view, name="logout"),
+    path('create', views.create, name="create"),
+    path('', views.red, name="create"),
+]
